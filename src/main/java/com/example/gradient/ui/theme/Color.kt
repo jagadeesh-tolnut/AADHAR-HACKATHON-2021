933821018ef8962fdbc78aae91a356faaf4be417
orange_coder <80932760+premPk2020@users.noreply.github.com>
2021-10-31 22:01:40 +0530

    Created gradient button

---
+++ src/main/java/com/example/gradient/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.gradient.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val upper = Color(0xFF0d0029)
+val lower = Color(0xFF00C4FF)
